3936cbbe414d6fcafaa849e3a44706f2964f6563
chalupaul <paul@paulsresume.net>
2014-01-24 19:15:29 -0600

    trying courier-BoldOblique

--- leaguefriend.rb
+++ leaguefriend.rb
@@ -238,7 +238,7 @@ watermark_text = Draw.new
 watermark_text.annotate(canvas, 0,0,398,0, text) do
   self.gravity = WestGravity
   self.pointsize = 14
-  self.font = "Courier-Bold"
+  self.font = "Courier-BoldOblique"
   self.fill = 'black'
   self.stroke = "none"
   self.align = RightAlign
